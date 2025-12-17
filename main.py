08474426736e277e0bcabd290b86c89d4868d317
Yiyao Zhang <yz5137@columbia.edu>
2025-12-17 03:13:40 -0500

    fix

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://storage.googleapis.com", 
-        "https://storage.googleapis.com/flipzy-frontend" 
     ],
     allow_credentials=True,
     allow_methods=["*"],
